8c1078bb4c5df54786247d8802c5eeb6ea18df65
probrahim <ibrahimhassani674@gmail.com>
2024-08-15 20:21:07 +0100

    1

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 """ function take the 1 arg and return floor"""
 
-import math
-
 
 def floor(n: float) -> int:
     """return  floor of n"""
-    return math.floor(n)
+    return int(n)
