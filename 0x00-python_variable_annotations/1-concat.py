33ac1d5f895690ed38935cd9c0ab4764a545c913
probrahim <ibrahimhassani674@gmail.com>
2024-08-15 18:35:55 +0100

    task 2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """ function take 2 arg and return de concat str1 + str2 """
 
 
-def concat(str1 : str, str2 : str) -> str :
+def concat(str1: str, str2: str) -> str:
     """ return concat """
     return str1 + str2
