6acd98fcf5865f63fd3838a1a67a61a7243ff95e
probrahim <ibrahimhassani674@gmail.com>
2024-09-06 20:58:00 +0100

    Module for testing client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+""" Module for testing client """
 
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
+import json
+import unittest
+from unittest.mock import patch, PropertyMock, Mock
